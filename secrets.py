c60fb753cd3739417e68f1d7fdf4b82ae2ff37b9
Mieszko Kaminski <mieszko.kaminski@icloud.com>
2017-05-17 20:04:27 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
-CLIENT_ID = 314063363929931777
-CLIENT_SECRET = "ZD9aEjcUqLPve-inwqb10c_qfxxWVxW0"
-BOT_TOKEN = "MzE0MDYzMzYzOTI5OTMxNzc3.C_yt0A.EzwW-GnYTsmpZi1txH88ArxOQoI"
+CLIENT_ID = client_id_goes_here
+CLIENT_SECRET = "client_token_goes_here"
+BOT_TOKEN = "bot_token_goes_here"
